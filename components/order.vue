c82ad50e51f910ace4d4810470809553e1366e39
thaycacac <thaycacac@gmail.com>
2019-06-17 16:11:35 +0700

    add: ui order

---
+++ components/order.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box columns">
+    <div class="column is-1">1</div>
+    <div class="column is-6">John Smith</div>
+    <div class="column is-1">10</div>
+    <div class="column is-1">$75</div>
+    <div class="column is-1">$750</div>
+    <div class="column is-2">
+      <div class="buttons has-addons">
+        <span class="button">Edit</span>
+        <span class="button">Delete</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box {
+  margin-top: 1rem;
+  margin-right: 0 !important;
+  margin-left: 0 !important;
+}
+</style>
---
+++ pages/cart/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <navbar />
+    <div class="container">
+      <div class="wrap-cart">
+        <div class="columns has-text-weight-bold header">
+          <div class="column is-1">#Id</div>
+          <div class="column is-6">Name</div>
+          <div class="column is-1">Quantity</div>
+          <div class="column is-1">Price</div>
+          <div class="column is-1">Amount</div>
+          <div class="column is-2">Action</div>
+        </div>
+        <order />
+        <order />
+        <order />
+        <div class="button is-primary is-large checkout">Checkout</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Product from '@/components/product'
+import Navbar from '@/components/navbar'
+import Order from '@/components/order'
+export default {
+  components: {
+    Product,
+    Navbar,
+    Order
+  }
+}
+</script>
+
+<style lang="scss">
+.wrap-cart {
+  margin-top: 2rem;
+  .checkout {
+    float: right;
+  }
+}
+</style>
